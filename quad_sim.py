4b677f3571a4afd369e51c196d95a0b4e60fdd1f
yunchang <yunchang@mit.edu>
2017-12-21 12:23:59 -0500

    Add files via upload
    added bebop controls

--- quad_sim.py
+++ quad_sim.py
@@ -3,8 +3,18 @@ import time
 import numpy as np
 
 
+from bebop_api_client import Bebop 
+
+def grid2bebop(x,y,theta):
+    # map descrete grid coordinates to bebop coord 
+    # (wall at 0)
+    b_x = (x-1)*0.3 # x_max in our space is 1.4
+    b_y = (y-1)*0.3 # y_max in our space is 2.5
+    b_thet = theta/360.*2.*np.pi # bebop takes radians? 
+    return [b_x, b_y, b_thet]
+
 class Simulator(object):
-    def __init__(self, X_DIM, Y_DIM, policy, model, init_state, grid_size=100):
+    def __init__(self, X_DIM, Y_DIM, policy, model, init_state, grid_size=100, bebop=False):
         # note the policy here is the sorted one
         self.master = Tk()
         w = X_DIM * grid_size + X_DIM + 1
@@ -24,6 +34,19 @@ class Simulator(object):
         self.policy = policy
         self.model = model
         self.current_state = init_state
+        self.bebop = bebop
+        if bebop:
+            self.quad = Bebop('quad')
+            self.guest = Bebop('guest') 
+            self.quad.takeoff()
+            self.guest.takeoff() 
+            # let quad be at height of 1 and guest be at height of 1
+            [qx, qy, qth] = grid2bebop(init_state[0][0], init_state[0][1], init_state[0][2])
+            qz = 1
+            [gx, gy, gth] = grid2bebop(init_state[1][0], init_state[1][1], init_state[1][2])
+            gz = 1
+            self.quad.fly_to(qx, qy, qz, theta=qth)
+            self.guest.fly_to(gx, gy, gz, theta=gth)
 
     def draw_grid(self):
         # grid line is black
@@ -73,6 +96,10 @@ class Simulator(object):
         [x3, y3] = [int(x + l * np.cos(thet + a)),
                     int(y - l * np.sin(thet + a))]
         self.C.coords(self.quad_poly, (x1, y1, x2, y2, x3, y3))
+        if self.bebop:
+            [q_x, q_y, q_thet] = grid2bebop(posx,posy,postheta)
+            # print(q_x, q_y, q_thet)
+            self.quad.fly_to(q_x, q_y, 1, theta=q_thet)
 
     def draw_guest(self, pose):  # ind corresponding pixel value
         (posx, posy, postheta, t) = pose  # draw the guest
@@ -105,6 +132,10 @@ class Simulator(object):
         [x3, y3] = [int(x + l * np.cos(thet + a)),
                     int(y - l * np.sin(thet + a))]
         self.C.coords(self.guest_poly, (x1, y1, x2, y2, x3, y3))
+        if self.bebop:
+            [g_x, g_y, g_thet] = grid2bebop(posx,posy,postheta)
+            # print(g_x, g_y, g_thet)
+            self.guest.fly_to(g_x, g_y, 1, theta=g_thet)
 
     def get_policy(self, combined_state):
         state2 = combined_state[1]
@@ -112,7 +143,6 @@ class Simulator(object):
 
     def update_new_state(self):
         act = self.policy[self.current_state[0]][self.current_state[1]]
-        print(act)
         newstate = self.model.state_transitions(
             self.current_state, act)[self.event]
         self.current_state = newstate[0]
